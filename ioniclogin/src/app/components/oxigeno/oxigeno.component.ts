022a949400136a095586d125300b34064b3d9667
Héctor Daza <hadazar@unal.edu.co>
2020-12-04 21:08:35 -0500

    separacion de graficas

---
+++ ioniclogin/src/app/components/oxigeno/oxigeno.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oxigeno',
+  templateUrl: './oxigeno.component.html',
+  styleUrls: ['./oxigeno.component.scss'],
+})
+export class OxigenoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
